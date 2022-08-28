f88e7db3fa5edefe62e94bcb4ee76cb0a4f438ec
Honest0 <jh4169306@gmail.com>
2022-08-28 19:50:28 +0200

    deploying script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,10 @@
 const hre = require("hardhat")
+require("dotenv").config()
+const { GEN_0_LIMIT } = process.env
 
 async function main() {
     const RobotCore = await hre.ethers.getContractFactory("RobotCore")
-    const robotCore = await RobotCore.deploy(40)
+    const robotCore = await RobotCore.deploy(GEN_0_LIMIT)
 
     console.log("RobotCore deployed to:", robotCore.address)
 }
